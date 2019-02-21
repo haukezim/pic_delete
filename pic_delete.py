ad9823400921771b84703dd48d6fd29798db3abd
haukezim <mail@haukezimmermann.de>
2019-02-21 17:43:08 +0100

    source file added

---
+++ pic_delete.py
@@ -0,0 +1,24 @@
+import sys
+import os
+
+def main():
+    if len(sys.argv) != 2:
+        print("You gave " + str(len(sys.argv)) + " arguments instead of only one")
+    else:
+        print("ok.")
+    if not os.path.exists(sys.argv[1]):
+        print("Path does not exist. :(")
+        return False
+    dir_path = os.path.realpath(sys.argv[1])
+    print(dir_path)
+    files = [file for file in os.listdir(dir_path) if os.path.isfile(os.path.join(dir_path, file))]
+    print(str(len(files)) + " files detected.")
+    files.sort()
+    print(files)
+
+    
+
+
+
+if __name__ == "__main__":
+    main()
